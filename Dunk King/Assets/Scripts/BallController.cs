a2728a8aa222a2a4f38ccd5120db930710965529
D-Dinchev <dincev.danil@gmail.com>
2022-07-26 17:49:16 +0300

    elevation effect added

--- Dunk King/Assets/Scripts/BallController.cs
+++ Dunk King/Assets/Scripts/BallController.cs
@@ -10,15 +10,26 @@ public class BallController : MonoBehaviour
     private Vector2 _dragStartPos;
     private float _maxVelocity = 9f;
 
+    private GameObject _stickedHoop;
+
+    private bool _isRotationActive = false;
+
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _lr = GetComponent<LineRenderer>();
+
+        GameEvents.Instance.OnRightHit += ToggleRotationActivity;
     }
 
     private void Update()
     {
-        if (_rb.velocity.magnitude > 0.001f) return;
+        if (_rb.velocity.magnitude > 0.001f)
+        {
+            if (_isRotationActive) transform.Rotate(0f, 0f, 5f);
+            return;
+        }
+
         
         if (Input.GetMouseButtonDown(0))
         {
@@ -46,6 +57,7 @@ public class BallController : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0))
         {
+            ToggleRotationActivity();
             _lr.positionCount = 0;
             Vector2 dragEndPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 velocity = (dragEndPos - (Vector2)transform.position) * Power * -1f;
@@ -79,5 +91,32 @@ public class BallController : MonoBehaviour
     private static Vector2 ClampVelocity(Vector2 velocity, float maxVelocity)
     {
         return velocity.magnitude > maxVelocity ? velocity.normalized * maxVelocity : velocity; 
+    }
+
+    public void StickToHoop(GameObject hoop)
+    {
+        if (_stickedHoop) return;
+
+        _stickedHoop = hoop;
+        transform.parent = _stickedHoop.transform;
+        //GetComponent<Rigidbody2D>().simulated = false;
+    }
+
+    public void UnstickToHoop()
+    {
+        if (!_stickedHoop) return;
+        transform.parent = null;
+        _stickedHoop = null;
+        //GetComponent<Rigidbody2D>().simulated = true;
+    }
+
+    private void ToggleRotationActivity()
+    {
+        _isRotationActive = !_isRotationActive;
+    }
+
+    private void OnDestroy()
+    {
+        GameEvents.Instance.OnRightHit -= ToggleRotationActivity;
     }
 }
--- Dunk King/Assets/Scripts/HoopGeneratorHandler.cs
+++ Dunk King/Assets/Scripts/HoopGeneratorHandler.cs
@@ -1,29 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class HoopGeneratorHandler : MonoBehaviour
 {
     public GameObject RedHoopPrefab;
+    public GameObject GreyHoopPrefab;
 
-    private float _minYDifference = 1f, _maxYDifference = 3f;
+    private float _minYDifference = 1f, _maxYDifference = 3.5f;
     //private float _maxRotationAnge = 34f;
 
     private Transform _bounds;
+
     private Vector3 _boundsCenter;
+    private Vector3 _startPosition;
     private Vector3 _rightCornerOfBounds;
     private Vector3 _leftCornerOfBounds;
-    private float _ySpawnPosition;
+
+    private float _ySpawnPoint;
 
     private float _halfOfHoopWidth;
     private float _minDistanceBetweenHoops;
+
+    private BallController _ballController;
     
 
     private void Start()
     {
         _bounds = GameObject.FindGameObjectWithTag("Bounds").transform;
         _boundsCenter = _bounds.transform.Find("Center").position;
+        _startPosition = _bounds.transform.Find("StartPosition").position;
+        _ySpawnPoint = _bounds.transform.Find("YSpawnPoint").position.y;
         _rightCornerOfBounds = _bounds.Find("Right").GetComponent<SpriteRenderer>().bounds.min;
         _leftCornerOfBounds = _bounds.Find("Left").GetComponent<SpriteRenderer>().bounds.max;
         GameObject topOfBounds = _bounds.transform.Find("Top").gameObject;
+
+        _ballController = GameObject.FindGameObjectWithTag("Player").GetComponent<BallController>();
 
         var sr = RedHoopPrefab.transform.Find("front").GetComponent<SpriteRenderer>();
         _halfOfHoopWidth = sr.bounds.extents.x;
@@ -39,7 +50,7 @@ public class HoopGeneratorHandler : MonoBehaviour
         float maxXPosition = _rightCornerOfBounds.x - _halfOfHoopWidth;
 
         float positionX = Random.Range(minXPosition, maxXPosition);
-        float positionY = Random.Range(currentRedHoop.position.y + _minYDifference, currentRedHoop.position.y + _maxYDifference);
+        float positionY = Random.Range(_startPosition.y + _minYDifference, _startPosition.y + _maxYDifference);
 
         if (currentRedHoop.position.x > _boundsCenter.x) // to the left of bounds center
         {
@@ -49,11 +60,34 @@ public class HoopGeneratorHandler : MonoBehaviour
             positionX = Random.Range(maxXPosition, minXPosition);
         }
 
-        if (RedHoopPrefab)
+        if (RedHoopPrefab && GreyHoopPrefab)
         {
-            
-            Instantiate(RedHoopPrefab, new Vector3(positionX, positionY, 0), Quaternion.identity);
+            GameObject newRedHoop = Instantiate(RedHoopPrefab, new Vector3(positionX, _ySpawnPoint, 0), Quaternion.identity);
+            GameObject newGreyHoop = Instantiate(GreyHoopPrefab, currentRedHoop.position, Quaternion.identity);
+            Destroy(currentRedHoop.gameObject);
+
+            StartCoroutine(LerpYPoint(newGreyHoop.transform, _startPosition.y, 1f));
+            StartCoroutine(LerpYPoint(newRedHoop.transform, positionY, 1f));
         }
+    }
+
+    private IEnumerator LerpYPoint(Transform objPosition, float endPosition, float duration)
+    {
+        float time = 0f;
+        Vector2 startPosition = objPosition.position;
+
+        _ballController.StickToHoop(objPosition.gameObject);
+
+        while (time < duration)
+        {
+            float newY = Mathf.Lerp(startPosition.y, endPosition, time / duration);
+            objPosition.position = new Vector3(objPosition.position.x, newY, transform.position.z);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        objPosition.position = new Vector3(objPosition.position.x, endPosition, transform.position.z);
+        _ballController.UnstickToHoop();
     }
 
     private void OnDestroy()
